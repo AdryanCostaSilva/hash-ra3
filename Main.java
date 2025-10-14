f56b98341675f24ce2540169d237e26c7cb4a0f9
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-10-14 15:13:13 -0300

    Created main

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+	Curso: Bacharelado em Ciência da Computação
+	Disciplina: Resolução de Problemas Estruturados em Computação
+	Professor: Andrey Cabral Meira
+	Período: 4
+	Turma: B
+	Integrantes:
+		-Adryan Costa Silva
+		-Hassan Ali El Gazouini
+		-Hussein Ali El Gazouini
+		-Murilo Zimerman Fortaleza
+*/
+public class Main{
+	public static void main(String[]args){
+		
+	}
+}
