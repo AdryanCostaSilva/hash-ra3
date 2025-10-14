acbc33fb2fee0cee998539312f30bfef82edaf0c
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-10-14 15:34:23 -0300

    Created seed and tested it

--- Main.java
+++ Main.java
@@ -10,8 +10,20 @@
 		-Hussein Ali El Gazouini
 		-Murilo Zimerman Fortaleza
 */
+import java.util.Random;
+
+
 public class Main{
 	public static void main(String[]args){
-		
+		Random random = new Random();
+		random.setSeed(999);
+		int list[] = new int[10];
+		for(int i = 0; i < 10; i++){
+			int n = random.nextInt(100000000, 1000000000);
+			list[i] = n;
+		}
+		for(int i = 0; i < 10; i++){
+			System.out.printf("%d\n", list[i]);
+		}
 	}
 }
