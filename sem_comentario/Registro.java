ad6684113e268fe027963ed6779f8d3d1ab112b8
haskaz313 <166724372+haskaz313@users.noreply.github.com>
2025-10-15 19:40:15 -0300

    Add files via upload

---
+++ sem_comentario/Registro.java
@@ -0,0 +1,13 @@
+
+public class Registro {
+    private Integer codigoRegistro;
+
+    public Registro(Integer codigoRegistro){
+        this.codigoRegistro = codigoRegistro;
+    }
+
+    public Integer getElemento(){
+        return codigoRegistro;
+    }
+
+}
